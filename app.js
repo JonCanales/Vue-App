1569ac51999d9cb6a4914badcd9b61bc2a73ee68
Jonathan <andriodjonathan1@gmail.com>
2020-05-24 20:26:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+var App = new Vue({
+el:"#Vue",
+data: {
+  health: 100,
+  ended: false
+
+},
+methods: {
+  punch: function(){
+    this.health -= 10;
+    if (this.health <= 0){
+      this.ended = true;
+    }
+  },
+  restart:function(){
+    this.health = 100;
+    this.ended = false;
+  }
+},
+computed:{
+
+
+}
+
+
+
+});
